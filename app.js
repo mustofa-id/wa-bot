120fa9db779d581e686916227c3020a597834b0c
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-09-14 09:07:47 +0700

    feat: add `!ffmpeg` command

--- app.js
+++ app.js
@@ -19,7 +19,27 @@ const config = {
 fs.mkdirSync(config.data_dir, { recursive: true });
 
 const db = new sqlite.DatabaseSync(path.join(config.data_dir, "db.sqlite"));
-const features = /** @type {const} */ (["!help", "!register", "!compress"]);
+const options = /** @type {const} */ ([
+	["!help", "Show help"],
+	["!register", "Register new user"],
+	["!compress", "Compress a attached document (video or image) for Status"],
+	[
+		"!ffmpeg",
+		"Run the *ffmpeg* command with the attached file. " +
+			"The first argument is the output file extension and " +
+			"the remaining arguments are *ffmpeg* parameters. For " +
+			"example: *!ffmpeg mp4 -r 30 -preset medium*.",
+	],
+	[
+		"!money",
+		"Track your income and expenses. " +
+			"Syntax: *!money <amount> <description>* " +
+			"Example: *!money -17000 Buy milk* or  *!money 150000 Got donation*. " +
+			"Note: Use a minus sign (-) for expenses. ",
+	],
+]);
+
+const features = options.map((o) => o[0]);
 
 /**
  * @typedef {typeof features[number]} Feature
@@ -87,9 +107,9 @@ async function handle_message(message) {
 	switch (feature) {
 		case "!help": {
 			await timers.setTimeout(2_000);
-			const commands = features.map((f) => `- ${f} <args> \n`).join("");
-			const info = `*Personal Bot* \n\nAvailable commands: \n${commands}`;
-			await message.reply(info);
+			const commands = options.map((f) => `- \`${f[0]}\` ${f[1]} \n`).join("");
+			const info = `🧰 *Multipurpose Tools*: \n\nAvailable commands: \n${commands}`;
+			await client.sendMessage(message.from, info);
 			break;
 		}
 
@@ -130,32 +150,8 @@ async function handle_message(message) {
 			await timers.setTimeout(1_000);
 			await message.reply("Please wait..");
 
-			if (!message.hasMedia) {
-				await timers.setTimeout(2_000);
-				await message.reply(`Was the attachment shy or just didn't vibe with the send button?`);
-				break;
-			}
-
-			if (message.type != wa.MessageTypes.DOCUMENT) {
-				await timers.setTimeout(2_000);
-				await message.reply(`I only accept "document", not digital doodles.`);
-				break;
-			}
-
-			const media = await message.downloadMedia();
-			if (!media) {
-				await timers.setTimeout(2_000);
-				await message.reply(`I opened it, saw nothing but disappointment. Care to try again?`);
-				break;
-			}
-
-			const is_image = media.mimetype.startsWith("image/");
-			const is_video = media.mimetype.startsWith("video/");
-			if (!is_image && !is_video) {
-				await timers.setTimeout(2_000);
-				await message.reply(`That file doesn't spark joy. Video or pic only, thanks.`);
-				break;
-			}
+			const media = await get_attached_doc(message, ["video", "image"]);
+			if (!media) break;
 
 			let need_times_to;
 			const need_times = setInterval(() => {
@@ -167,14 +163,14 @@ async function handle_message(message) {
 
 			/** @type {wa.Message | undefined} */
 			let result_message;
-			if (is_video) {
+			if (media.type == "video") {
 				const result = await convert_video(media.data);
 				const video = wa.MessageMedia.fromFilePath(result.output);
 				result_message = await client.sendMessage(message.from, video);
 				cleanup(result.dir);
 			}
 
-			if (is_image) {
+			if (media.type == "image") {
 				const result = await convert_image(media.data);
 				const image = wa.MessageMedia.fromFilePath(result.output);
 				result_message = await client.sendMessage(message.from, image, { sendMediaAsHd: true });
@@ -195,16 +191,73 @@ async function handle_message(message) {
 
 			break;
 		}
+
+		case "!ffmpeg": {
+			await timers.setTimeout(1_000);
+			await message.reply("Please wait..");
+
+			const media = await get_attached_doc(message);
+			if (!media) break;
+
+			const [ext, ...cmd_args] = args;
+			const result = await ffmpeg({ base64: media.data, ext: `.${ext}`, cmd_args });
+			const content = wa.MessageMedia.fromFilePath(result.output);
+			await message.reply(content, undefined, {
+				sendMediaAsDocument: true,
+				caption: "Here you go!",
+			});
+
+			break;
+		}
+
+		// TODO: !money
+
+		default:
+			await timers.setTimeout(3_000);
+			await message.reply(`Hang tight, this command's not working just yet.`);
+			break;
 	}
+}
+
+/**
+ * @param {wa.Message} message
+ * @param {('video' | 'image' | 'audio' | 'unknown')[]} filters
+ */
+async function get_attached_doc(message, filters = []) {
+	await timers.setTimeout(2_000);
+	if (!message.hasMedia) {
+		await message.reply(`Was the attachment shy or just didn't vibe with the send button?`);
+		return;
+	}
+
+	if (message.type != wa.MessageTypes.DOCUMENT) {
+		await message.reply(`I only accept "document", not digital doodles.`);
+		return;
+	}
+
+	const media = await message.downloadMedia();
+	if (!media) {
+		await message.reply(`I opened it, saw nothing but disappointment. Care to try again?`);
+		return;
+	}
+
+	// there are more types like 'application' or 'text' but we don't use it yet
+	const [type] = /** @type {typeof filters} */ (
+		media.mimetype.split(";")?.[0]?.trim()?.toLowerCase()?.split("/") || ["unknown"]
+	);
+
+	if (filters?.length && !filters.includes(type)) {
+		const allowed = filters.map((t) => `'${t}'`).join(" or ");
+		await message.reply(`That file doesn't spark joy. Only ${allowed}, Thanks.`);
+		return;
+	}
+
+	return { ...media, type };
 }
 
 /** @param {string} base64 video file in base64 */
 async function convert_video(base64) {
 	const args = [
-		"-y", // overwrite
-		"-hide_banner",
-		["-loglevel", "error"],
-
 		// Ensure fast-start for web/social media
 		["-movflags", "+faststart"],
 
@@ -228,7 +281,7 @@ async function convert_video(base64) {
 		["-ac", "2"],
 	];
 
-	return convert_media({
+	return ffmpeg({
 		base64,
 		ext: ".mp4",
 		cmd_args: args.flat(),
@@ -238,10 +291,6 @@ async function convert_video(base64) {
 /** @param {string} base64 image file in base64 */
 async function convert_image(base64) {
 	const args = [
-		"-y",
-		"-hide_banner",
-		["-loglevel", "error"],
-
 		// Ensure we output exactly one frame for animated sources (GIF/A-PNG)
 		["-frames:v", "1"],
 
@@ -254,7 +303,7 @@ async function convert_image(base64) {
 		["-map_metadata", "-1"],
 	];
 
-	return convert_media({ base64, ext: ".jpg", cmd_args: args.flat() });
+	return ffmpeg({ base64, ext: ".jpg", cmd_args: args.flat() });
 }
 
 /** @param {string} dir */
@@ -268,9 +317,9 @@ async function cleanup(dir) {
 
 /**
  *
- * @param {{ base64: string; ext: `.${string}`; cmd_args: string[] }} params
+ * @param {{ base64: string; ext: `.${string}`; cmd_args?: string[] }} params
  */
-async function convert_media(params) {
+async function ffmpeg(params) {
 	const id = crypto.randomUUID();
 	const dir = await fsp.mkdtemp(path.join(os.tmpdir(), "media-"));
 	const input = path.join(dir, `${id}.bin`);
@@ -278,7 +327,8 @@ async function convert_media(params) {
 
 	await fsp.writeFile(input, Buffer.from(params.base64, "base64"));
 
-	const args = [["-i", input], ...(params.cmd_args || []), output];
+	const default_args = ["-y" /* overwrite */, "-hide_banner", ["-loglevel", "error"]];
+	const args = [...default_args, ["-i", input], ...(params.cmd_args || []), output];
 	await new Promise((resolve, reject) => {
 		const ffmpeg = proc.spawn(`ffmpeg`, args.flat(), { windowsHide: true });
 		let error_output = "";
