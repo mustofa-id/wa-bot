d044ba4238b2405005fb14c1c4d664a3c805bf17
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-09-15 09:43:53 +0700

    fix: migrations fail

--- migrations/002.sql
+++ migrations/002.sql
@@ -1,3 +1,9 @@
+-- We need to do this so adding the created_at column won’t fail, 
+-- since SQLite doesn’t allow non-constant default values if there’s
+-- already data in the table. This isn’t really a problem since there 
+-- wasn’t any specific relation to the users table before.
+delete from users where true;
+
 alter table users add column created_at text not null default (current_timestamp);
 
 alter table users add column is_active integer not null default 1;
